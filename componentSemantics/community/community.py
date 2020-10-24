5901bea0df8203bfafd0b2a6302e6bf310921609
Cezar Sas <cezar.sas@gmail.com>
2020-10-24 17:50:18 +0200

    Separated community detection algorithms and cleanup

---
+++ componentSemantics/community/community.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+import leidenalg
+from igraph import Graph
+import infomap
+
+
+class AbstractCommunityDetection(ABC):
+    @abstractmethod
+    def find_community(self, graph: Graph, weights="weight"):
+        raise not NotImplemented
+
+
+class Leiden(AbstractCommunityDetection):
+    def find_community(self, graph: Graph, weights="weight"):
+        return leidenalg.find_partition(graph, leidenalg.ModularityVertexPartition, weights=weights).membership
+
+
+class Infomap(AbstractCommunityDetection):
+    def __init__(self, params="--directed --silent --seed 1337 --num-trials 10"):
+        self.params = params
+
+    def find_community(self, graph: Graph, weights="weight"):
+        im = infomap.Infomap(self.params)
+
+        for edge in graph.es:
+            im.add_link(edge.source, edge.target, edge[weights])
+
+        im.run()
+
+        unsorted_communitites = []
+        for node in im.tree:
+            if node.is_leaf:
+                unsorted_communitites.append((node.node_id, node.module_id))
+        min_id = min([x[1] for x in unsorted_communitites])
+        communities = [x[1] - min_id for x in sorted(unsorted_communitites, key=lambda x: x[0])]
+
+        return communities
--- componentSemantics/community_extraction.py
+++ componentSemantics/community_extraction.py
@@ -1,29 +1,23 @@
 import glob
 import os
+import random
 import shutil
 from collections import Counter
+from typing import Dict
 
 import igraph
-import leidenalg
+import matplotlib.pyplot as plt
 import numpy
 import seaborn
-import random
-import matplotlib.pyplot as plt
-
-from infomap import Infomap
-
 from tqdm import tqdm
 
+from community.community import Leiden, Infomap, AbstractCommunityDetection
 from utils import check_dir, clean_graph
 
 
 class CommunityExtraction:
-    def __init__(self):
-        self.algorithms = {
-            "leiden": self.leiden,
-            "infomap": self.infomap
-        }
-
+    def __init__(self, algorithms: Dict[str, AbstractCommunityDetection]):
+        self.algorithms = algorithms
         self.visual_style = {"vertex_size": 15, "bbox": (1000, 1000), "margin": 20}
 
     def extract(self, project_name, graph_path, out_path):
@@ -35,64 +29,33 @@ class CommunityExtraction:
         plot_out = os.path.join(out_path, "plots", "graphs")
         check_dir(plot_out)
         print("Project", project_name, "# Nodes", len(graph.vs), "# Edges", len(graph.es))
+
+        size_out = os.path.join(out_path, "plots", "size")
+        check_dir(size_out)
+
         for method in self.algorithms:
             method_out = os.path.join(out_path, "graphs", method, "raw", project_name)
             check_dir(method_out)
-            communities = self.algorithms[method](graph)
+            communities = self.algorithms[method].find_community(graph)
             graph.vs[method] = communities
-            sub_communities = list(self._extract_sub(graph, method))
 
-            pal = igraph.drawing.colors.ClusterColoringPalette(len(set(graph.vs[method])))
-            graph.vs['color'] = pal.get_many(graph.vs[method])
             self.plot(graph, plot_out, f"{project_name}_{method}.pdf", method)
+
             connected_components, dependency, weighted_dependency = self.extract_community_dependency(graph, method)
-            name = f"comm_dependencies_{method}.csv"
-            numpy.savetxt(os.path.join(graph_out, name), dependency.astype(int), fmt='%i', delimiter=",")
+            self.save_dependencies(dependency, graph_out, f"comm_dependencies_{method}.csv")
+            self.save_dependencies(weighted_dependency, graph_out, f"comm_dependencies_weighted_{method}.csv")
 
-            name = f"comm_dependencies_weighted_{method}.csv"
-            numpy.savetxt(os.path.join(graph_out, name), weighted_dependency.astype(int), fmt='%i', delimiter=",")
-
+            sub_communities = list(self._extract_sub(graph, method))
             for i, community in enumerate(sub_communities):
                 name = f"comm_{i}.graphml"
                 self.save_graph(community, method_out, name)
 
             print("Project", project_name, "Method", method, "# Comm", len(set(communities)))
 
-            counts = Counter(communities)
-            ax = plt.axes()
-            sizes = [x[1] for x in counts.most_common(100)]
-            print(sizes)
-            seaborn.regplot(x=list(range(len(sizes))),
-                            y=[x[1] for x in counts.most_common(100)],
-                            scatter_kws={"s": 80},
-                            order=3, ci=None)
-            ax.set_title(f"{project_name} - {method}")
-            plt.show()
+            self.plot_size_distribution(communities, size_out, f"{project_name}_{method}.pdf")
 
         name = f"{project_name}.graphml"
         self.save_graph(graph, graph_out, name)
-
-    @staticmethod
-    def leiden(graph):
-        return leidenalg.find_partition(graph, leidenalg.ModularityVertexPartition, weights="weight").membership
-
-    @staticmethod
-    def infomap(graph):
-        im = Infomap("--directed --silent --seed 1337 --num-trials 10")
-
-        for edge in graph.es:
-            im.add_link(edge.source, edge.target, edge["weight"])
-
-        im.run()
-
-        unsorted_communitites = []
-        for node in im.tree:
-            if node.is_leaf:
-                unsorted_communitites.append((node.node_id, node.module_id))
-        min_id = min([x[1] for x in unsorted_communitites])
-        communities = [x[1] - min_id for x in sorted(unsorted_communitites, key=lambda x: x[0])]
-
-        return communities
 
     @staticmethod
     def _extract_sub(graph, method):
@@ -107,9 +70,13 @@ class CommunityExtraction:
         graph.write_graphml(out)
 
     @staticmethod
+    def save_dependencies(dependencies, path, name):
+        numpy.savetxt(os.path.join(path, name), dependencies.astype(int), fmt='%i', delimiter=",")
+
+    @staticmethod
     def extract_community_dependency(graph, method):
         connected = set()
-        n = len(set(graph.vs[method]))
+        n = graph.vcount()
         dependency = numpy.zeros((n, n))
         weighted_dependency = numpy.zeros((n, n))
 
@@ -125,17 +92,38 @@ class CommunityExtraction:
         return connected, dependency, weighted_dependency
 
     def plot(self, graph, path, name, method):
+        pal = igraph.drawing.colors.ClusterColoringPalette(len(set(graph.vs[method])))
+        graph.vs['color'] = pal.get_many(graph.vs[method])
         layout = graph.layout_fruchterman_reingold()
         self.visual_style["layout"] = layout
         self.visual_style["vertex_label"] = graph.vs[method]
         igraph.plot(graph, os.path.join(path, name), **self.visual_style)
+        del (graph.vs['color'])
+
+    @staticmethod
+    def plot_size_distribution(communities, graph_out, name):
+        counts = Counter(communities)
+        sizes = [x[1] for x in counts.most_common(100)]
+        print(sizes)
+
+        seaborn.regplot(x=list(range(len(sizes))),
+                        y=[x[1] for x in counts.most_common(100)],
+                        scatter_kws={"s": 80},
+                        order=3, ci=None)
+
+        plt.savefig(os.path.join(graph_out, name))
 
 
 def extract_communities(in_path, out_path):
     projects = [project for project in os.listdir(in_path)
                 if os.path.isdir(os.path.join(in_path, project))]
 
-    extractor = CommunityExtraction()
+    algorithms = {
+        "leiden": Leiden(),
+        "infomap": Infomap()
+    }
+
+    extractor = CommunityExtraction(algorithms)
 
     for project in tqdm(projects):
         filepath = glob.glob(os.path.join(in_path, project, "dep-graph-*.graphml"))[0]
